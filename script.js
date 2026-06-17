9d13005f99d7d5a74791d23fa78f9ba5ee46cb4e
pian <ff0020320@gmail.com>
2026-06-17 16:42:52 +0700

    Update targetAmount in CONFIG to 10300000

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  targetAmount: 8100000,
+  targetAmount: 10300000,
   monthlySaving: 500000,
   supabaseUrl: "https://ggjmxzeuzcrkobfzkhgy.supabase.co",
   supabaseAnonKey: "sb_publishable_-j4pRJs6YWWFfNG7shEeEg_zb5wGzZX",
